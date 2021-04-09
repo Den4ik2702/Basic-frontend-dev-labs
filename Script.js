9e319205b79bdc9c826abe7cc5f686a0aca9d542
Den4ik <deniel270202@gmail.com>
2021-04-09 20:23:30 +0300

    Added preloader

---
+++ Script.js
@@ -0,0 +1,14 @@
+function knopka() {
+
+    Swal.fire( {
+        allowEscapeKey:'true',
+        background:'#FFEFD5',
+        title:'Внимание!',
+        iconColor:'#FF6347',
+        icon:'info',
+        text:'Макет был реализован Буториным Дэниелем Михайловичем. Вариант № 9',
+        confirmButtonColor:'#FF4500',
+        confirmButtonText:'Ok'
+    })
+
+}
